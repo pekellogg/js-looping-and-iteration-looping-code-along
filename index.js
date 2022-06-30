d54b9eee4a5e99bde365623c7f934321cfe87f5d
Peyton Kellogg <pekellogg@hotmail.com>
2022-06-30 14:32:38 -0700

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(friends, event) {
+    let thanksFriends = []
+    for (let i = 0; i < friends.length; i++) {
+        thanksFriends.push(`Thank you, ${friends[i]}, for the wonderful ${event} gift!`);
+    }
+    return thanksFriends;
+}
+
+function countDown(number) {
+    for(number; number >= 0; number--) {
+        console.log(number);
+    }
+}
